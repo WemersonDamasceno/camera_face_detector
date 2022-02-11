85314c02d0a128615ba48ff49170a3b6a566e67d
Wemerson Damasceno <wemersondamasceno7@gmail.com>
2022-02-11 13:01:42 -0300

    Dividindo em arquivos

--- lib/face_detector_page.dart
+++ lib/face_detector_page.dart
@@ -1,19 +1,15 @@
 // ignore_for_file: avoid_print
-
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:camera/camera.dart';
 import 'package:camera_process/camera_process.dart';
 
-import 'main.dart';
+import 'camera_view.dart';
 
 int qtdFoto = 0;
 bool isFotoEsquerda = false;
 bool isFotoDireita = false;
 bool isFotoFrente = false;
-
-CameraController? _controller;
 
 class FaceDetectorView extends StatefulWidget {
   const FaceDetectorView({Key? key}) : super(key: key);
@@ -55,11 +51,7 @@ class _FaceDetectorViewState extends State<FaceDetectorView> {
     final faces = await faceDetector.processImage(inputImage);
 
     if (qtdFoto > 0) {
-      // const snackBar = SnackBar(
-      //   content: Text('SORRIA! ☺'),
-      // );
-      // ScaffoldMessenger.of(context).showSnackBar(snackBar);
-      final imagem = await _controller?.takePicture();
+      //final imagem = await _controller?.takePicture();
     }
 
     if (faces.length > 1) {
@@ -82,123 +74,6 @@ class _FaceDetectorViewState extends State<FaceDetectorView> {
     if (mounted) {
       setState(() {});
     }
-  }
-}
-
-class CameraView extends StatefulWidget {
-  const CameraView(
-      {Key? key,
-      required this.title,
-      required this.customPaint,
-      required this.onImage,
-      this.initialDirection = CameraLensDirection.front})
-      : super(key: key);
-
-  final String title;
-  final CustomPaint? customPaint;
-  final Function(InputImage inputImage) onImage;
-  final CameraLensDirection initialDirection;
-
-  @override
-  _CameraViewState createState() => _CameraViewState();
-}
-
-class _CameraViewState extends State<CameraView> {
-  final int _cameraIndex = 1;
-
-  @override
-  void initState() {
-    super.initState();
-    _startLiveFeed();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: _liveFeedBody(),
-    );
-  }
-
-  Widget _liveFeedBody() {
-    if (_controller?.value.isInitialized == false) {
-      return Container();
-    }
-    return Container(
-      color: Colors.black,
-      child: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          CameraPreview(_controller!),
-          if (widget.customPaint != null) widget.customPaint!,
-        ],
-      ),
-    );
-  }
-
-  Future _startLiveFeed() async {
-    final camera = cameras[_cameraIndex];
-    _controller = CameraController(
-      camera,
-      ResolutionPreset.low,
-      enableAudio: false,
-    );
-    _controller?.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
-      _controller?.startImageStream(_processCameraImage);
-      setState(() {});
-    });
-  }
-
-  Future _processCameraImage(CameraImage image) async {
-    final WriteBuffer allBytes = WriteBuffer();
-    for (Plane plane in image.planes) {
-      allBytes.putUint8List(plane.bytes);
-    }
-    final bytes = allBytes.done().buffer.asUint8List();
-
-    final Size imageSize =
-        Size(image.width.toDouble(), image.height.toDouble());
-
-    final camera = cameras[_cameraIndex];
-    final imageRotation =
-        InputImageRotationMethods.fromRawValue(camera.sensorOrientation) ??
-            InputImageRotation.Rotation_0deg;
-
-    final inputImageFormat =
-        InputImageFormatMethods.fromRawValue(image.format.raw) ??
-            InputImageFormat.NV21;
-
-    final planeData = image.planes.map(
-      (Plane plane) {
-        return InputImagePlaneMetadata(
-          bytesPerRow: plane.bytesPerRow,
-          height: plane.height,
-          width: plane.width,
-        );
-      },
-    ).toList();
-
-    final inputImageData = InputImageData(
-      size: imageSize,
-      imageRotation: imageRotation,
-      inputImageFormat: inputImageFormat,
-      planeData: planeData,
-    );
-
-    final inputImage =
-        InputImage.fromBytes(bytes: bytes, inputImageData: inputImageData);
-
-    widget.onImage(inputImage);
   }
 }
 
@@ -238,12 +113,12 @@ class FaceDetectorPainter extends CustomPainter {
             //Quando virar o rosto pra direita ou pra esquerda
             if (qtdFoto == 0) {
               if (point.dx < 80) {
-                qtdFoto = 1;
+                //qtdFoto = 1;
                 print("Moveu para a direita: $point");
                 print("QTD: $qtdFoto");
               } else if (point.dx > 150) {
                 print("Moveu para a esquerda: $point");
-                qtdFoto++;
+                //qtdFoto = 1;
                 print("QTD: $qtdFoto");
                 //imagem = await _controller?.takePicture();
               }
