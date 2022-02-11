a3b6d606b773fadbfa7e966556bbc9258cc177df
Wemerson Damasceno <wemersondamasceno7@gmail.com>
2022-02-11 11:00:32 -0300

    Criando repositorio

---
+++ lib/face_detector_page.dart
@@ -0,0 +1,320 @@
+// ignore_for_file: avoid_print
+
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:camera/camera.dart';
+import 'package:camera_process/camera_process.dart';
+
+import 'main.dart';
+
+int qtdFoto = 0;
+bool isFotoEsquerda = false;
+bool isFotoDireita = false;
+bool isFotoFrente = false;
+
+CameraController? _controller;
+
+class FaceDetectorView extends StatefulWidget {
+  const FaceDetectorView({Key? key}) : super(key: key);
+
+  @override
+  _FaceDetectorViewState createState() => _FaceDetectorViewState();
+}
+
+class _FaceDetectorViewState extends State<FaceDetectorView> {
+  FaceDetector faceDetector =
+      CameraProcess.vision.faceDetector(const FaceDetectorOptions(
+    enableContours: true,
+    enableClassification: true,
+  ));
+  bool isBusy = false;
+  CustomPaint? customPaint;
+
+  @override
+  void dispose() {
+    faceDetector.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CameraView(
+      title: 'Face Detector',
+      customPaint: customPaint,
+      onImage: (inputImage) {
+        processImage(inputImage);
+      },
+      initialDirection: CameraLensDirection.front,
+    );
+  }
+
+  Future<void> processImage(InputImage inputImage) async {
+    if (isBusy) return;
+    isBusy = true;
+    final faces = await faceDetector.processImage(inputImage);
+
+    if (qtdFoto > 0) {
+      // const snackBar = SnackBar(
+      //   content: Text('SORRIA! ☺'),
+      // );
+      // ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      final imagem = await _controller?.takePicture();
+    }
+
+    if (faces.length > 1) {
+      const snackBar = SnackBar(
+        content: Text('Existe mais de uma pessoa na frente da camera!'),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+    if (inputImage.inputImageData?.size != null &&
+        inputImage.inputImageData?.imageRotation != null) {
+      final painter = FaceDetectorPainter(
+          faces,
+          inputImage.inputImageData!.size,
+          inputImage.inputImageData!.imageRotation);
+      customPaint = CustomPaint(painter: painter);
+    } else {
+      customPaint = null;
+    }
+    isBusy = false;
+    if (mounted) {
+      setState(() {});
+    }
+  }
+}
+
+class CameraView extends StatefulWidget {
+  const CameraView(
+      {Key? key,
+      required this.title,
+      required this.customPaint,
+      required this.onImage,
+      this.initialDirection = CameraLensDirection.front})
+      : super(key: key);
+
+  final String title;
+  final CustomPaint? customPaint;
+  final Function(InputImage inputImage) onImage;
+  final CameraLensDirection initialDirection;
+
+  @override
+  _CameraViewState createState() => _CameraViewState();
+}
+
+class _CameraViewState extends State<CameraView> {
+  final int _cameraIndex = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    _startLiveFeed();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: _liveFeedBody(),
+    );
+  }
+
+  Widget _liveFeedBody() {
+    if (_controller?.value.isInitialized == false) {
+      return Container();
+    }
+    return Container(
+      color: Colors.black,
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          CameraPreview(_controller!),
+          if (widget.customPaint != null) widget.customPaint!,
+        ],
+      ),
+    );
+  }
+
+  Future _startLiveFeed() async {
+    final camera = cameras[_cameraIndex];
+    _controller = CameraController(
+      camera,
+      ResolutionPreset.low,
+      enableAudio: false,
+    );
+    _controller?.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      _controller?.startImageStream(_processCameraImage);
+      setState(() {});
+    });
+  }
+
+  Future _processCameraImage(CameraImage image) async {
+    final WriteBuffer allBytes = WriteBuffer();
+    for (Plane plane in image.planes) {
+      allBytes.putUint8List(plane.bytes);
+    }
+    final bytes = allBytes.done().buffer.asUint8List();
+
+    final Size imageSize =
+        Size(image.width.toDouble(), image.height.toDouble());
+
+    final camera = cameras[_cameraIndex];
+    final imageRotation =
+        InputImageRotationMethods.fromRawValue(camera.sensorOrientation) ??
+            InputImageRotation.Rotation_0deg;
+
+    final inputImageFormat =
+        InputImageFormatMethods.fromRawValue(image.format.raw) ??
+            InputImageFormat.NV21;
+
+    final planeData = image.planes.map(
+      (Plane plane) {
+        return InputImagePlaneMetadata(
+          bytesPerRow: plane.bytesPerRow,
+          height: plane.height,
+          width: plane.width,
+        );
+      },
+    ).toList();
+
+    final inputImageData = InputImageData(
+      size: imageSize,
+      imageRotation: imageRotation,
+      inputImageFormat: inputImageFormat,
+      planeData: planeData,
+    );
+
+    final inputImage =
+        InputImage.fromBytes(bytes: bytes, inputImageData: inputImageData);
+
+    widget.onImage(inputImage);
+  }
+}
+
+class FaceDetectorPainter extends CustomPainter {
+  FaceDetectorPainter(this.faces, this.absoluteImageSize, this.rotation);
+
+  final List<Face> faces;
+  final Size absoluteImageSize;
+  final InputImageRotation rotation;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 2
+      ..color = Colors.purple;
+
+    for (final Face face in faces) {
+      //Desenhar o retangulo
+      canvas.drawRect(
+        Rect.fromLTRB(
+          translateX(face.boundingBox.left, rotation, size, absoluteImageSize),
+          translateY(face.boundingBox.top, rotation, size, absoluteImageSize),
+          translateX(face.boundingBox.right, rotation, size, absoluteImageSize),
+          translateY(
+              face.boundingBox.bottom, rotation, size, absoluteImageSize),
+        ),
+        paint,
+      );
+
+      void paintContour(FaceContourType type) async {
+        final faceContour = face.getContour(type);
+
+        for (Offset point in faceContour!.positionsList) {
+          //Se o nariz for pro canto direito
+          if (type == FaceContourType.noseBottom) {
+            //Quando virar o rosto pra direita ou pra esquerda
+            if (qtdFoto == 0) {
+              if (point.dx < 80) {
+                qtdFoto = 1;
+                print("Moveu para a direita: $point");
+                print("QTD: $qtdFoto");
+              } else if (point.dx > 150) {
+                print("Moveu para a esquerda: $point");
+                qtdFoto++;
+                print("QTD: $qtdFoto");
+                //imagem = await _controller?.takePicture();
+              }
+            }
+          }
+          //Pintar os pontos
+
+          // canvas.drawCircle(
+          //     Offset(
+          //       translateX(point.dx, rotation, size, absoluteImageSize),
+          //       translateY(point.dy, rotation, size, absoluteImageSize),
+          //     ),
+          //     1,
+          //     paint);
+        }
+      }
+
+      paintContour(FaceContourType.noseBottom);
+
+      //paintContour(FaceContourType.face);
+      // paintContour(FaceContourType.leftEyebrowTop);
+      // paintContour(FaceContourType.leftEyebrowBottom);
+      // paintContour(FaceContourType.rightEyebrowTop);
+      // paintContour(FaceContourType.rightEyebrowBottom);
+      // paintContour(FaceContourType.leftEye);
+      // paintContour(FaceContourType.rightEye);
+      // paintContour(FaceContourType.upperLipTop);
+      // paintContour(FaceContourType.upperLipBottom);
+      // paintContour(FaceContourType.lowerLipTop);
+      // paintContour(FaceContourType.lowerLipBottom);
+      // paintContour(FaceContourType.noseBridge);
+      // paintContour(FaceContourType.leftCheek);
+      // paintContour(FaceContourType.rightCheek);
+    }
+  }
+
+  @override
+  bool shouldRepaint(FaceDetectorPainter oldDelegate) {
+    return oldDelegate.absoluteImageSize != absoluteImageSize ||
+        oldDelegate.faces != faces;
+  }
+}
+
+double translateX(
+    double x, InputImageRotation rotation, Size size, Size absoluteImageSize) {
+  switch (rotation) {
+    case InputImageRotation.Rotation_90deg:
+      return x *
+          size.width /
+          (Platform.isIOS ? absoluteImageSize.width : absoluteImageSize.height);
+    case InputImageRotation.Rotation_270deg:
+      return size.width -
+          x *
+              size.width /
+              (Platform.isIOS
+                  ? absoluteImageSize.width
+                  : absoluteImageSize.height);
+    default:
+      return x * size.width / absoluteImageSize.width;
+  }
+}
+
+double translateY(
+    double y, InputImageRotation rotation, Size size, Size absoluteImageSize) {
+  switch (rotation) {
+    case InputImageRotation.Rotation_90deg:
+    case InputImageRotation.Rotation_270deg:
+      return y *
+          size.height /
+          (Platform.isIOS ? absoluteImageSize.height : absoluteImageSize.width);
+    default:
+      return y * size.height / absoluteImageSize.height;
+  }
+}
